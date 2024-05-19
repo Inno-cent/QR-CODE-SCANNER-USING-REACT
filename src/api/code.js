8b9eac46db5ad4e74dc943a0036408986e15b6df
innocent <ekweozorinnocent@gmail.com>
2024-05-19 17:49:18 +0100

    added api

---
+++ src/api/code.js
@@ -0,0 +1,22 @@
+const qr = require("qrcode");
+
+export default function handler(req, res) {
+  if(!req.body || !req.body.data){
+    return res.status(500).send('No request body or data')
+  }
+
+  const {name, surname, email, dob, phone, timestamp} = req?.body?.data
+  if(!(name && surname && email && dob && phone && timestamp)){
+    return res.status(500).send('Missing data, please fill-in all the inputs fields')
+  }
+
+  qr.toDataURL(JSON.stringify(req.body.data), { maskPattern: req.body.mask || 0 })
+    .then(url => {
+      return res.status(200).send(url)
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).send('Something broke!')
+    })
+  
+}
